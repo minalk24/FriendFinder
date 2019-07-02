bbb74bebdb73c1578a8a0af1a12e89b494d190aa
Minal Kharche <minal.berkeley@gmail.com>
2019-07-02 15:19:52 -0700

    updated with saved friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,69 @@
 
 
 var friendsArray = [
-    {
-        name: "Kit Harington",
-        photoUrl: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1368,w_2031,x_0,y_132/v1554733971/shape/mentalfloss/568598-gettyimages-468057150.jpg?itok=89YwA3PD",
-        answers:[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-    }, 
-    {
-      name: "Emilia Clarke",
-      photoUrl: "https://nerdist.com/wp-content/uploads/2019/04/Emilia-Clarke-1200x676.jpg",
-      answers:[
-          5,
-          1,
-          4,
-          4,
-          5,
-          1,
-          2,
-          5,
-          4,
-          2
-        ]
+  {
+    name: "Kit Harington",
+    photoUrl: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1368,w_2031,x_0,y_132/v1554733971/shape/mentalfloss/568598-gettyimages-468057150.jpg?itok=89YwA3PD",
+    answers: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: "Emilia Clarke",
+    photoUrl: "https://nerdist.com/wp-content/uploads/2019/04/Emilia-Clarke-1200x676.jpg",
+    answers: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      2
+    ]
+  },
+  {
+    name: "Jaime",
+    photoUrl: "https://timesofindia.indiatimes.com/thumb/msid-66364134,imgsize-37342,width-800,height-600,resizemode-4/66364134.jpg",
+    answers: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      5
+    ]
+  },
+  {
+    name: "Sansa",
+    photoUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/4/4b/Sophie_Turner_2019.jpg/revision/latest?cb=20190513214537",
+    answers: [
+      4,
+      1,
+      4,
+      4,
+      5,
+      1,
+      3,
+      5,
+      4,
+      5
+    ]
   }
 ];
 
