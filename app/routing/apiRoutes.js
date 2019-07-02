64e2794b5ee5ef61925726f1034a4d3c20b9eca8
Minal Kharche <minal.berkeley@gmail.com>
2019-07-01 22:36:40 -0700

    added routing using get and post method

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,50 @@
 //---------------------------------------------------------------
 //All required packages
 //---------------------------------------------------------------
-var friends = require("../data/friends");
+var friendsData = require("../data/friends");
 
 //---------------------------------------------------------------
 //Routing
 //---------------------------------------------------------------
-module.exports = function(app){
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
 
-    app.get("/api/friends", function(req, res){
-        res.json(friends);
-    })
+  app.get("/api/match", function (req, res) {
+    var minMatchFactor = Number.MAX_VALUE;
+    var matchIndex = -1;
 
-    app.post("/api/friends", function(req, res){
-        var newMatchFriend = req.body;
+    for (var i = 0; i < friendsData.length - 1; i++) {
 
-        friends.push(newMatchFriend);
-        return res.json(newMatchFriend);
-    })
-}
+      matchFactor =
+        Math.abs(friendsData[friendsData.length - 1].answers[0] - friendsData[i].answers[0]) + Math.abs(friendsData[friendsData.length - 1].answers[1] - friendsData[i].answers[1]) + Math.abs(friendsData[friendsData.length - 1].answers[2] - friendsData[i].answers[2]) + Math.abs(friendsData[friendsData.length - 1].answers[3] - friendsData[i].answers[3]) + Math.abs(friendsData[friendsData.length - 1].answers[4] - friendsData[i].answers[4]) + Math.abs(friendsData[friendsData.length - 1].answers[5] - friendsData[i].answers[5]) + Math.abs(friendsData[friendsData.length - 1].answers[6] - friendsData[i].answers[6]) + Math.abs(friendsData[friendsData.length - 1].answers[7] - friendsData[i].answers[7]) + Math.abs(friendsData[friendsData.length - 1].answers[8] - friendsData[i].answers[8]) + Math.abs(friendsData[friendsData.length - 1].answers[9] - friendsData[i].answers[9]);
+
+      //get the minimum matchFactor
+      if (minMatchFactor >= matchFactor) {
+        minMatchFactor = matchFactor;
+        matchIndex = i;
+      }
+    }
+    
+    res.send(friendsData[matchIndex]);
+  });
+
+  app.post("/api/friends", function (req, res) {
+
+    var answersArray = [];
+
+    for (var i = 0; i < req.body.answers.length; i++)
+      answersArray.push(parseInt(req.body.answers[i]));
+
+    var newfriend = {
+      name: req.body.name,
+      photoURL: req.body.photoURL,
+      answers: answersArray
+    };
+
+    friendsData.push(newfriend);
+    res.json(true);
+  });
+
+};
