a0f1adda223dc9f333439704fbd87d23a631f79f
Minal Kharche <minal.berkeley@gmail.com>
2019-07-09 01:25:43 -0700

    updated with new friend charcter

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,6 @@
 
 var friendsArray = [
   {
-    name: "Kit Harington",
-    photoUrl: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1368,w_2031,x_0,y_132/v1554733971/shape/mentalfloss/568598-gettyimages-468057150.jpg?itok=89YwA3PD",
-    answers: [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-  },
-  {
     name: "Emilia Clarke",
     photoUrl: "https://nerdist.com/wp-content/uploads/2019/04/Emilia-Clarke-1200x676.jpg",
     answers: [
@@ -63,6 +47,22 @@ var friendsArray = [
       5,
       4,
       5
+    ]
+  },
+  {
+    name: "Kit Harington",
+    photoUrl: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1368,w_2031,x_0,y_132/v1554733971/shape/mentalfloss/568598-gettyimages-468057150.jpg?itok=89YwA3PD",
+    answers: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
     ]
   }
 ];
